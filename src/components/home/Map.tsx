259f2699be46c3490ade3222709f41893683557e
HaniElhamwi <hanialhamwi000000@gmail.com>
2025-06-25 00:42:35 +0300

    updated site

--- src/components/home/Map.tsx
+++ src/components/home/Map.tsx
@@ -19,7 +19,7 @@ const Map = async () => {
             frameBorder="0"
             title="map"
             scrolling="no"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3047.998308947094!2d29.003818015537917!3d41.06556737929538!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cab7d4f7af3937%3A0xec6f288f2f21ebdb!2sCam%20Pak%20otopark%20ve%20y%C4%B1kama!5e0!3m2!1str!2str!4v1687364546074!5m2!1str!2str"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d302.51021461731897!2d29.0040037!3d41.0655793!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cab7e0e90d1005%3A0x1bfb0e4fa8b47755!2sCam%20Pak%20otopark%20ve%20y%C4%B1kama!5e0!3m2!1sen!2str!4v1719338412345!5m2!1sen!2str"
             style={{ minHeight: "100%" }}
           />
           {/* Contact Info Overlay (visible on larger screens) */}
@@ -58,7 +58,7 @@ const Map = async () => {
 
                 <Link
                   target="_blank"
-                  href="https://www.google.com.tr/maps/place/Cam+Pak..."
+                  href="https://www.google.com/maps/place/Cam+Pak+otopark+ve+y%C4%B1kama/@41.0655793,29.0040037,21z/data=!4m14!1m7!3m6!1s0x14cab654199f9f09:0xc56988360e82a6aa!2zR2F5cmV0dGVwZSwgVmVmYWJleSBTb2thxJ_EsSBObzoyNSBEOjEsIDM0MzQ5IEJlxZ9pa3RhxZ8vxLBzdGFuYnVs!3b1!8m2!3d41.0656118!4d29.0042239!3m5!1s0x14cab7e0e90d1005:0x1bfb0e4fa8b47755!8m2!3d41.0656188!4d29.0039287!16s%2Fg%2F11y1dv3j2v?entry=ttu&g_ep=EgoyMDI1MDYxNy4wIKXMDSoASAFQAw%3D%3D"
                   className="flex items-center gap-2 text-sm text-secondary font-montserrat group hover:text-secondary transition-colors duration-200">
                   <MapPin size={18} className="text-primary" />
                   <span className="underline group-hover:no-underline">
@@ -101,7 +101,7 @@ const Map = async () => {
             </Link>
             <Link
               target="_blank"
-              href="https://www.google.com.tr/maps/place/Cam+Pak..."
+              href="https://www.google.com/maps/place/Cam+Pak+otopark+ve+y%C4%B1kama/@41.0655793,29.0040037,21z/data=!4m14!1m7!3m6!1s0x14cab654199f9f09:0xc56988360e82a6aa!2zR2F5cmV0dGVwZSwgVmVmYWJleSBTb2thxJ_EsSBObzoyNSBEOjEsIDM0MzQ5IEJlxZ9pa3RhxZ8vxLBzdGFuYnVs!3b1!8m2!3d41.0656118!4d29.0042239!3m5!1s0x14cab7e0e90d1005:0x1bfb0e4fa8b47755!8m2!3d41.0656188!4d29.0039287!16s%2Fg%2F11y1dv3j2v?entry=ttu&g_ep=EgoyMDI1MDYxNy4wIKXMDSoASAFQAw%3D%3D"
               className="flex items-center gap-2 text-sm text-secondary font-montserrat group hover:text-secondary transition-colors duration-200">
               <MapPin size={18} className="text-primary" />
               <span className="underline group-hover:no-underline">
