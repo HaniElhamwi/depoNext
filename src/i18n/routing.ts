be086343be98503f3cd8c10f61aa700e32a6e7c6
HaniElhamwi <hanialhamwi000000@gmail.com>
2025-07-05 00:10:23 +0300

    delete locales

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,6 +1,6 @@
 import { defineRouting } from "next-intl/routing";
 
-export const locales = ["en", "ar", "tr"] as const;
+export const locales = ["tr"] as const;
 export const routing = defineRouting({
   locales,
   defaultLocale: "tr",
